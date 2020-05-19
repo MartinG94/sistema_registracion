aad494a6dff85dff74c2d6e6ba0ce436a7385f96
MartinG94 <guillenmartin94@gmail.com>
2020-05-19 11:50:42 -0300

    Peticion por get a router para /register

--- ejercitacion/routes/index.js
+++ ejercitacion/routes/index.js
@@ -4,5 +4,6 @@ const mainController = require('../controller/mainController');
 
 /* GET home page. */
 router.get('/', mainController.home);
+router.get('/register', mainController.register);
 
 module.exports = router;
