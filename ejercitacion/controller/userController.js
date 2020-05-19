1384485508063879f7f048c417c55b02d7a0d061
MartinG94 <guillenmartin94@gmail.com>
2020-05-19 13:24:11 -0300

    Metodos necesarios para el funcionamiento del controller

--- ejercitacion/controller/userController.js
+++ ejercitacion/controller/userController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 const controller = {
     root: (req, res, next) => {
@@ -7,10 +8,30 @@ const controller = {
             title: 'Usuarios'
         });
     },
-    register: (req, res) => {
+    formRegister: (req, res) => {
         res.render('register', {
             title: 'Registro'
         });
+    },
+    register: (req, res, next) => {
+        const body = req.body;
+        if(body.password != body.repeat_password){
+            res.send('Las contraseñas no coinciden!');
+        };
+        const listaUsuarios = JSON.parse(fs.readFileSync('data/usuarios.json'));
+        const cantidadUsuarios = listaUsuarios.length;
+        const nuevoUsuario = {
+            nombre: body.nombre,
+            apellido: body.apellido,
+            email: body.email,
+            password: body.password,
+        };
+        listaUsuarios.push(nuevoUsuario);
+        fs.writeFileSync('data/usuarios.json', JSON.stringify(listaUsuarios));
+        res.send('Registro con Éxito');
+    },
+    profile: (req, res) => {
+        res.send('Perfil!');
     }
 };
 
